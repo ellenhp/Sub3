badfb770453273d56e0c2a0f3e83bf92ab4667c5
Nolan Poe <nopoe.dev@gmail.com>
2015-05-24 12:22:10 -0700

    added GPL comment header to all source files

--- server/SubServer.cpp
+++ server/SubServer.cpp
@@ -1,3 +1,21 @@
+/*
+ * Sub^3 is a free software submarine simulator focused on realism.
+ * Copyright (C) 2015 Nolan Poe
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "SubServer.hpp"
 
 #include "Sub3.hpp"
